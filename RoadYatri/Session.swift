a91a6397230e37ba8725d8b6f7583efd5271b6bd
Deepak <deepak.kadarivel@gmail.com>
2016-04-12 17:29:41 +0530

    Home Design stage 2

--- RoadYatri/Session.swift
+++ RoadYatri/Session.swift
@@ -11,33 +11,21 @@ import UIKit
 class Session {
     
     var title: String
-    var speaker: String
-    var room: String
-    var time: String
+    var subs: String
     var backgroundImage: UIImage
     
-    var roomAndTime: String {
-        get {
-            return "\(time) • \(room)"
-        }
-    }
-    
-    init(title: String, speaker: String, room: String, time: String, backgroundImage: UIImage) {
+    init(title: String,  subs: String, backgroundImage: UIImage) {
         self.title = title
-        self.speaker = speaker
-        self.room = room
-        self.time = time
+        self.subs = subs
         self.backgroundImage = backgroundImage
     }
     
     convenience init(dictionary: NSDictionary) {
         let title = dictionary["Title"] as? String
-        let speaker = dictionary["Speaker"] as? String
-        let room = dictionary["Room"] as? String
-        let time = dictionary["Time"] as? String
+        let subs = dictionary["Subs"] as? String
         let backgroundName = dictionary["Background"] as? String
         let backgroundImage = UIImage(named: backgroundName!)
-        self.init(title: title!, speaker: speaker!, room: room!, time: time!, backgroundImage: backgroundImage!.decompressedImage)
+        self.init(title: title!, subs: subs!, backgroundImage: backgroundImage!.decompressedImage)
     }
     
 }
--- RoadYatri/TimelineCollectionViewCell.swift
+++ RoadYatri/TimelineCollectionViewCell.swift
@@ -14,15 +14,13 @@ class TimelineCollectionViewCell: UICollectionViewCell {
     @IBOutlet private weak var imageCoverView: UIView!
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var timeAndRoomLabel: UILabel!
-    @IBOutlet private weak var speakerLabel: UILabel!
     
     var destination: Destination? {
         didSet {
             if let destination = destination {
                 imageView.image = destination.backgroundImage
                 titleLabel.text = destination.title
-                timeAndRoomLabel.text = destination.roomAndTime
-                speakerLabel.text = destination.speaker
+                timeAndRoomLabel.text = destination.subs
             }
         }
     }
@@ -38,13 +36,12 @@ class TimelineCollectionViewCell: UICollectionViewCell {
         let delta = 1 - ((featuredHeight - CGRectGetHeight(frame)) / (featuredHeight - standardHeight))
         
         // 3
-        let minAlpha: CGFloat = 0.1
+        let minAlpha: CGFloat = 0.3
         let maxAlpha: CGFloat = 0.75
         imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
         
         let scale = max(delta, 0.5)
         titleLabel.transform = CGAffineTransformMakeScale(scale, scale)
         timeAndRoomLabel.alpha = delta
-        speakerLabel.alpha = delta
     }
 }
